c1a2ad0b11e780acf917eea093c0feac518c1198
Jonathan Niehof <jniehof@lanl.gov>
2011-01-07 08:49:02 -0700

    Generic bits of DBFormatter into base classes; implement DBFormatter.regex
     * Create QuietFormatter, AssemblingFormatter
     * This is perhaps a bit inheritance-mad
     * regex: replaces special fields with regular expressions

--- DBStrings.py
+++ DBStrings.py
@@ -12,6 +12,39 @@ __version__ = '0.0'
 import string
 
 
+class AssemblingFormatter(string.Formatter):
+    """String formatter extended with a method to reassemble field specs"""
+
+    @staticmethod
+    def assemble(literal, field, format, conversion):
+        """Assembles components of a field specification
+
+        Converse of parse. Takes literal text, field name, format spec,
+        and conversion and assembles into a full field spec.
+
+        @param literal: any literal text preceding the field definition
+        @type literal: str
+        @param field: name of the field
+        @type field: str
+        @param format: format specification to apply to L{field}
+        @type format: str
+        @param conversion: conversion to apply to L{field}
+        @type conversion: str
+        @return: a full format spec that will parse into L{literal},
+                 L{field}, L{format}, L{conversion}
+        @rtype: str
+        """
+        fs = literal + '{' + field
+        if conversion:
+            fs += ('!' + conversion)
+        if format:
+            if literal or field or conversion:
+                fs += ':'
+            fs += format
+        fs += '}'
+        return fs
+
+
 class _UnfoundField(object):
     """Information on a field specified in format string, but not in arguments
 
@@ -55,18 +88,17 @@ class _UnfoundField(object):
         @param format_spec: format specification for this field
         @type format_spec: str
         """
-        fs = '{' + self.key + self.lookups
-        if self.conversion is not None:
-            fs += ('!' + self.conversion)
-        if format_spec:
-            fs += (':' + format_spec)
-        fs += '}'
-        return fs
+        return QuietFormatter.assemble('', self.key + self.lookups,
+                                       format_spec, self.conversion)
 
-    
-class DBFormatter(string.Formatter):
-    """String formatter extended/modified for DBUtils"""
 
+class QuietFormatter(AssemblingFormatter):
+    """String formatter extended to silently leave unfilled fields alone.
+
+    Normal formatter throws an exception if a field cannot be filled from
+    the provided argument; this one simply retains the original field spec.
+    """
+    #Follow four methods extend base formatter class
     def get_field(self, field_name, args, kwargs):
         """Find object referenced by a field_name
 
@@ -74,7 +106,7 @@ class DBFormatter(string.Formatter):
         the field.
         """
         obj, first = \
-             super(DBFormatter, self).get_field(field_name, args, kwargs)
+             super(QuietFormatter, self).get_field(field_name, args, kwargs)
         if isinstance(obj, _UnfoundField):
             return obj, None
         else:
@@ -87,7 +119,7 @@ class DBFormatter(string.Formatter):
         which don't exist, rather than throwing an exception.
         """
         try:
-            return super(DBFormatter, self).get_value(key, *args, **kwargs)
+            return super(QuietFormatter, self).get_value(key, *args, **kwargs)
         except KeyError:
             return _UnfoundField(key)
 
@@ -101,7 +133,7 @@ class DBFormatter(string.Formatter):
             value.conversion = conversion
             return value
         else:
-            return super(DBFormatter, self).convert_field(value, conversion)
+            return super(QuietFormatter, self).convert_field(value, conversion)
 
     def format_field(self, value, format_spec):
         """Creates final formatted field
@@ -111,34 +143,81 @@ class DBFormatter(string.Formatter):
         if isinstance(value, _UnfoundField):
             return value.field_spec(format_spec)
         else:
-            return super(DBFormatter, self).format_field(value, format_spec)
+            return super(QuietFormatter, self).format_field(value, format_spec)
+
+
+class DBFormatter(QuietFormatter):
+    """String formatter extended/modified for DBUtils
+
+    @cvar SPECIAL_FIELDS: indexed by field name; each element contains
+                          a fully-formatted representation of the field
+                          and a regular expression that should match it.
+    @type SPECIAL_FIELDS: dict
+    """
+    SPECIAL_FIELDS = {
+        'Y': ('{Y:04d}', '(19|2\d)\d\d'),
+        'm': ('{m:02d}', '(0\d|1[0-2])'),
+        'd': ('{d:02d}', '[0-3]\d'),
+        'y': ('{y:02d}', '\d\d'),
+        'j': ('{j:03d}', '[0-3]\d\d'),
+        'H': ('{H:02d}', '[0-2]\d'),
+        'M': ('{M:02d}', '[0-6]\d'),
+        'MILLI': ('{MILLI:03d}', '\d{3}'),
+        'MICRO': ('{MICRO:03d}', '\d{3}'),
+        'QACODE': ('{QACODE}', '(ok|ignore|problem)'),
+        }
 
     #Following methods provide new functionality for this class
-    @staticmethod
-    def assemble(literal, field, format, conversion):
-        """Assembles components of a field specification
+    def expand_format(self, format_string):
+        """Adds formatting codes to 'special' fields in format string
 
-        Converse of parse. Takes literal text, field name, format spec,
-        and conversion and assembles into a full field spec.
+        For every field defined in L{SPECIAL_FIELDS}, if there is no format
+        spec nor conversion specified, replace it on the output with the
+        full format spec in L{SPECIAL_FIELDS}. Everything else is returned
+        verbatim.
 
-        @param literal: any literal text preceding the field definition
-        @type literal: str
-        @param field: name of the field
-        @type field: str
-        @param format: format specification to apply to L{field}
-        @type format: str
-        @param conversion: conversion to apply to L{field}
-        @type conversion: str
-        @return: a full format spec that will parse into L{literal},
-                 L{field}, L{format}, L{conversion}
+        @param format_string: the format string to convert
+        @type format_string: str
+        @return: L{format_string} with the fields defined in L{SPECIAL_FIELDS}
+                 expanded to full format specifiers.
         @rtype: str
         """
-        fs = literal + '{' + field
-        if conversion:
-            fs += ('!' + conversion)
-        if format:
-            if literal or field or conversion:
-                fs += ':'
-            fs += format
-        fs += '}'
-        return fs
+
+        result = []
+        for literal, field, format, conversion in self.parse(format_string):
+            if field in self.SPECIAL_FIELDS and (not format) and (not conversion):
+                result.append(literal)
+                result.append(self.SPECIAL_FIELDS[field][0])
+            else:
+                result.append(self.assemble(literal, field, format, conversion))
+        return ''.join(result)
+
+    def regex(self, format_string):
+        """Convert 'special' fields in format string to regex
+
+        For every field defined in L{SPECIAL_FIELDS}, if there is no
+        forma spec/conversion specified OR it matches that in
+        L{SPECIAL_FIELDS}, replace with the regular expression from
+        L{SPECIAL_FIELDS}. Everything else returned verbatim.
+
+  
+        @param format_string: the format string to convert
+        @type format_string: str
+        @return: L{format_string} with the fields defined in L{SPECIAL_FIELDS}
+                 replaced with matching regular expressions
+        @rtype: str
+        """
+
+        result = []
+        for literal, field, format, conversion in self.parse(format_string):
+            if field in self.SPECIAL_FIELDS:
+                result.append(literal)
+                orig = self.assemble('', field, format, conversion)
+                if (not format and not conversion) \
+                   or self.SPECIAL_FIELDS[field][0] == orig:
+                    result.append(self.SPECIAL_FIELDS[field][1])
+                else:
+                    result.append(orig)
+            else:
+                result.append(self.assemble(literal, field, format, conversion))
+        return ''.join(result)      
--- testDBStrings.py
+++ testDBStrings.py
@@ -10,41 +10,17 @@ import unittest
 import DBStrings
 
 
-class DBFormatterTests(unittest.TestCase):
-    """Tests of the revised Formatter class
-
-    @ivar fmtr: instance of the revised formatter
-    @type fmtr: DBStrings.DBFormatter
-    """
-
-    def __init__(self, *args, **kwargs):
-        """Create a formatter object"""
-        super(DBFormatterTests, self).__init__(*args, **kwargs)
-        self.fmtr = DBStrings.DBFormatter()
-
-    def testNormalFormatting(self):
-        """Format some strings that are same as normal formatter"""
-        self.assertEqual('hi there',
-                         self.fmtr.format('hi {a}', a='there'))
-        self.assertEqual('hi there',
-                         self.fmtr.format('hi {0}', 'there'))
-        self.assertEqual('0003.20 hi',
-                         self.fmtr.format('{1:07.2f} {0}', 'hi', 3.2))
-
-    def testMissingKey(self):
-        """Format strings with unspecified keys"""
-        self.assertEqual('hi {there}',
-                         self.fmtr.format('{hi} {there}', hi='hi'))
-        self.assertEqual(
-            'hi {there.here!s:100s}',
-            self.fmtr.format('{hi} {there.here!s:100s}', hi='hi'))
+class AssemblingFormatterTests(unittest.TestCase):
+    """Test of the Formatter which reassembles field specs"""
 
     def testAssemble(self):
         """Assemble components of a field spec"""
-        self.assertEqual('{04d}',
-                         self.fmtr.assemble('', '', '04d', ''))
-        self.assertEqual('stuff{name[0]!s:4.2f}',
-                         self.fmtr.assemble('stuff', 'name[0]', '4.2f', 's'))
+        self.assertEqual(
+            '{04d}',
+            DBStrings.AssemblingFormatter.assemble('', '', '04d', ''))
+        self.assertEqual(
+            'stuff{name[0]!s:4.2f}',
+            DBStrings.AssemblingFormatter.assemble('stuff', 'name[0]', '4.2f', 's'))
 
 
 class UnfoundFieldTests(unittest.TestCase):
@@ -81,6 +57,57 @@ class UnfoundFieldTests(unittest.TestCase):
         a.conversion = 's'
         self.assertEqual("{key['keyname'].attribute[0]!s:04d}",
                          a.field_spec('04d'))
+
+class QuietFormatterTests(unittest.TestCase):
+    """Tests of the QuietFormatter"""
+
+    def __init__(self, *args, **kwargs):
+        """Create a formatter"""
+        super(QuietFormatterTests, self).__init__(*args, **kwargs)
+        self.fmtr = DBStrings.QuietFormatter()
+
+    def testNormalFormatting(self):
+        """Format some strings that are same as normal formatter"""
+        self.assertEqual('hi there',
+                         self.fmtr.format('hi {a}', a='there'))
+        self.assertEqual('hi there',
+                         self.fmtr.format('hi {0}', 'there'))
+        self.assertEqual('0003.20 hi',
+                         self.fmtr.format('{1:07.2f} {0}', 'hi', 3.2))
+
+    def testMissingKey(self):
+        """Format strings with unspecified keys"""
+        self.assertEqual('hi {there}',
+                         self.fmtr.format('{hi} {there}', hi='hi'))
+        self.assertEqual(
+            'hi {there.here!s:100s}',
+            self.fmtr.format('{hi} {there.here!s:100s}', hi='hi'))
+
+
+class DBFormatterTests(unittest.TestCase):
+    """Tests of the revised Formatter class
+
+    @ivar fmtr: instance of the revised formatter
+    @type fmtr: DBStrings.DBFormatter
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Create a formatter"""
+        super(DBFormatterTests, self).__init__(*args, **kwargs)
+        self.fmtr = DBStrings.DBFormatter()
+
+    def testExpandFormat(self):
+        """Add formatting codes to special fields"""
+        self.assertEqual('{Y:04d}',
+                         self.fmtr.expand_format('{Y}'))
+        self.assertEqual(
+            'stuff{morestuff|r:55.4f}{Y:02d}{Y:04d}',
+            self.fmtr.expand_format('stuff{morestuff|r:55.4f}{Y:02d}{Y}'))
+
+    def testRegex(self):
+        """Replace special fields with regular expression"""
+        self.assertEqual('stuff\d{3}[0-3]\d\d{d:2d}',
+                         self.fmtr.regex('stuff{MILLI}{j:03d}{d:2d}'))
 
 
 if __name__ == '__main__':
