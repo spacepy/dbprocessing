d0619c2e787d9c6abad4f6253108ff436158e789
Brian Larsen <balarsen@lanl.gov>
2012-07-03 16:10:23 -0600

    writeProductsConf: intital cut a config file writer from the db

---
+++ scripts/writeProductsConf.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+#!/usr/bin/env python2.6
+
+
+# following the recipe in the document I wrote
+
+
+#==============================================================================
+# INPUTS
+#==============================================================================
+# mission name
+# satellite name
+# product name
+## prod name
+## product rel path
+## prod filename format
+## prod level
+# <- add the prod
+# <- create the inst_prod link
+
+import ConfigParser
+from dateutil import parser as dup
+import sys
+
+from dbprocessing import DBUtils2
+from dbprocessing import Version
+
+sections = ['base', 'product', 'inspector',]
+
+
+
+def usage():
+    """
+    print the usage messag out
+    """
+    print "Usage: {0} <product name> <filename>".format(sys.argv[0])
+    print "   -> product name (or number) to write to config file"
+    return
+
+def toBool(value):
+    if value in ['True', 'true', True, 1, 'Yes', 'yes']:
+        return True
+    else:
+        return False
+
+def toNone(value):
+    if value == '':
+        return None
+    else:
+        return value
+
+def writeconfig(my_cfg, config_filepath):
+    cfg=ConfigParser.ConfigParser()
+    for section in my_cfg:
+        cfg.add_section(section)
+        for key in my_cfg[section]:
+            cfg.set(section, key, my_cfg[section][key])
+    fp=open(config_filepath, "wb")
+    cfg.write(fp)
+    fp.close()
+    return
+
+
+def getStuff(prod_name, filename):
+    cfg = {}
+    dbu = DBUtils2.DBUtils2('rbsp') # TODO don't assume RBSP later
+    print("Connected to DB")
+    dbu._openDB()
+    dbu._createTableObjects()
+    cfg['mission'] = {}
+    cfg['mission']['name'] = dbu.mission
+    cfg['mission']['directory'] = dbu._getMissionDirectory()
+
+    writeconfig(cfg, )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 3:
+        usage()
+        sys.exit(2)
+    getStuff(sys.argv[1], sys.srgv[2])
+
