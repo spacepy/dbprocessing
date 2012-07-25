03b2b761ee6fd7db8fba0bf5a7e90878d61efd00
Brian Larsen <balarsen@lanl.gov>
2012-07-25 12:52:31 -0600

    writeDBhtml.py: major cleanup of the product output and added product_description to html

--- scripts/writeDBhtml.py
+++ scripts/writeDBhtml.py
@@ -55,69 +55,131 @@ def makeHTML(mission, filename):
     output.close()
 
 
+#def _writeProducts(dbu, output):
+#    prods = dbu.getProducts()
+#    data = {}
+#    keys1 = ['level', 'product_name', 'product_description', 'relative_path', 'format']
+#    for key in keys1:
+#        data[key] = [v.__getattribute__(key) for v in prods]
+#
+#    keys2 = ['instrument_name']
+#    for key in keys2:
+#        data[key] = [dbu.getProductTraceback(v.product_id)['instrument'].__getattribute__(key) for v in prods]
+#
+#    keys3 = ['satellite_name']
+#    for key in keys3:
+#        data[key] = [dbu.getProductTraceback(v.product_id)['satellite'].__getattribute__(key) for v in prods]
+#
+#    keys4 = ['n_files']
+#    for key in keys4:
+#        data[key] = [len(dbu.getFilesByProduct(v.product_id)) for v in prods]
+#
+#    keys5 = ['latest']
+#    for key in keys5:
+#        data[key] = []
+#        for p in prods:
+#            tmp = dbu.getFilesByProduct(p.product_id)
+#            tmp = [t.utc_stop_time for t in tmp]
+#            tmp.sort()
+#            try:
+#                data[key].append(tmp[-1].isoformat())
+#            except IndexError:
+#                data[key].append(None)
+#
+#    keys = keys3 + keys2 + keys1[:-1] + keys4 + keys5 + [keys1[-1]]
+#
+#    # go through and sort the data by several keys
+#    ind = np.lexsort( (data['product_name'], data['level'], data['instrument_name'], data['satellite_name'],   ) )
+#
+#    output.write('<h1>{0}</h1>\n'.format(dbu.mission))
+#    output.write('<h2>{0}</h2>\n'.format(datetime.datetime.now().isoformat()))
+#
+#    output.write('<h2>{0}</h2>\n'.format('Products'))
+#
+#    output.write('<table style="border: medium none ; border-collapse: collapse;" border="0" cellpadding="0" cellspacing="0">\n')
+#
+#    output.write('<tr>')
+#    for attr in keys:
+#        output.write('<th>{0}</th>'.format(attr))
+#    output.write('</tr>\n')
+#    for i, idx in enumerate(ind):
+#        if i % 2 == 0:
+#            output.write('<tr>')
+#        else:
+#            output.write('<tr class="alt">')
+#        output.write('<td>{0}</td>'.format(data['satellite_name'][idx]))
+#        output.write('<td>{0}</td>'.format(data['instrument_name'][idx]))
+#        output.write('<td>{0}</td>'.format(data['level'][idx]))
+#        output.write('<td>{0}</td>'.format(data['product_name'][idx]))
+#        output.write('<td>{0}</td>'.format(data['relative_path'][idx]))
+#        output.write('<td>{0}</td>'.format(data['n_files'][idx]))
+#        output.write('<td>{0}</td>'.format(data['latest'][idx]))
+#        output.write('<td>{0}</td>'.format(data['format'][idx]))
+#        output.write('</tr>\n')
+#
+#    output.write('</table>\n')
+
 def _writeProducts(dbu, output):
-    prods = dbu.getProducts()
+    prods = dbu.getAllProducts()
+
+    traceback = []
+    for prod in prods:
+        traceback.append(dbu.getProductTraceback(prod.product_id))
+
     data = {}
-    keys1 = ['level', 'product_name', 'relative_path', 'format']
-    for key in keys1:
-        data[key] = [v.__getattribute__(key) for v in prods]
 
-    keys2 = ['instrument_name']
-    for key in keys2:
-        data[key] = [dbu.getProductTraceback(v.product_id)['instrument'].__getattribute__(key) for v in prods]
+    data['satellite_name'] = []
+    data['instrument_name'] = []
+    data['level'] = []
+    data['product_name'] = []
+    data['product_description'] = []
+    data['relative_path'] = []
+    data['n_files'] = []
+    data['format'] = []
 
-    keys3 = ['satellite_name']
-    for key in keys3:
-        data[key] = [dbu.getProductTraceback(v.product_id)['satellite'].__getattribute__(key) for v in prods]
+    keys = ['satellite_name', 'instrument_name', 'level', 'product_name',
+            'product_description', 'relative_path', 'n_files', 'format', ]
 
-    dt = [datetime.datetime(1950, 1, 1), datetime.datetime(2050, 1, 1)]
+    for tb in traceback:
+        for key in keys:
+            if key == 'satellite_name':
+                data[key].append(tb['satellite'].satellite_name)
+            elif key == 'instrument_name':
+                data[key].append(tb['instrument'].instrument_name)
+            elif key == 'level':
+                data[key].append(tb['product'].level)
+            elif key == 'product_name':
+                data[key].append(tb['product'].product_name)
+            elif key == 'product_description':
+                data[key].append(tb['product'].product_description)
+            elif key == 'relative_path':
+                data[key].append(tb['product'].relative_path)
+            elif key == 'format':
+                data[key].append(tb['product'].format)
+            elif key == 'n_files':
+                data[key].append(len(dbu.getFilesByProduct(tb['product'].product_id)))
 
-    keys4 = ['n_files']
-    for key in keys4:
-        data[key] = [len(dbu.getFilesByProduct(v.product_id)) for v in prods]
-
-    keys5 = ['latest']
-    for key in keys5:
-        data[key] = []
-        for p in prods:
-            tmp = dbu.getFilesByProduct(p.product_id)
-            tmp = [t.utc_stop_time for t in tmp]
-            tmp.sort()
-            try:
-                data[key].append(tmp[-1].isoformat())
-            except IndexError:
-                data[key].append(None)
-
-    keys = keys3 + keys2 + keys1[:-1] + keys4 + keys5 + [keys1[-1]]
-
-    # go through and sort the data by several keys
+    # go through and sort the data by several keys (right to left, ugg)
     ind = np.lexsort( (data['product_name'], data['level'], data['instrument_name'], data['satellite_name'],   ) )
 
-    output.write('<h1>{0}</h1>\n'.format(dbu.mission))
-    output.write('<h2>{0}</h2>\n'.format(datetime.datetime.now().isoformat()))
 
-    output.write('<h2>{0}</h2>\n'.format('Products'))
+    output.write('<h2>{0}</h2>\n'.format('Processes'))
 
     output.write('<table style="border: medium none ; border-collapse: collapse;" border="0" cellpadding="0" cellspacing="0">\n')
-
+    # write out the header
     output.write('<tr>')
     for attr in keys:
         output.write('<th>{0}</th>'.format(attr))
     output.write('</tr>\n')
+
+    # and write all the data
     for i, idx in enumerate(ind):
         if i % 2 == 0:
             output.write('<tr>')
         else:
             output.write('<tr class="alt">')
-        output.write('<td>{0}</td>'.format(data['satellite_name'][idx]))
-        output.write('<td>{0}</td>'.format(data['instrument_name'][idx]))
-        output.write('<td>{0}</td>'.format(data['level'][idx]))
-        output.write('<td>{0}</td>'.format(data['product_name'][idx]))
-        output.write('<td>{0}</td>'.format(data['relative_path'][idx]))
-        output.write('<td>{0}</td>'.format(data['n_files'][idx]))
-        output.write('<td>{0}</td>'.format(data['latest'][idx]))
-        output.write('<td>{0}</td>'.format(data['format'][idx]))
-        output.write('</tr>\n')
+        for key in keys:
+            output.write('<td>{0}</td>'.format(data[key][idx]))
 
     output.write('</table>\n')
 
