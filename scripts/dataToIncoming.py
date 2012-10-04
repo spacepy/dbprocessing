1ce034b5fea95d885d14c104bf4fc228cf94a198
Brian Larsen <balarsen@lanl.gov>
2012-10-04 17:13:06 -0600

    dataToIncoming.py: production version 1 of this code

--- scripts/dataToIncoming.py
+++ scripts/dataToIncoming.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python2.6
 
+import itertools
 import os
 import tempfile
 import shutil
 import subprocess
+
 
 import dbprocessing.DBUtils as DBUtils
 import dbprocessing.DBlogging as DBlogging
@@ -15,28 +17,41 @@ mission_path = dbu.getMissionDirectory()
 inc_path = dbu.getIncomingPath()
 data_path = os.path.expanduser(os.path.join('~ectsoc', 'data', 'level_0'))
 
-data_path_repta = os.path.join(data_path, 'a', 'rept')
-miss_path_repta = os.path.join(mission_path, 'rbspa', 'rept', 'level0')
+def sync_data(sc, inst):
+    global mission_path
+    global inc_path
+    global data_path
+    data_path_inst = os.path.join(data_path, sc.lower(), inst.lower())
+    miss_path_inst = os.path.join(mission_path, 'rbsp'+sc.lower(),
+                                  inst, 'level0')
 
-curdir = os.path.abspath(os.curdir)
-tmp_path = tempfile.mkdtemp(suffix='_dbprocessing')
-os.chdir(tmp_path)
+    curdir = os.path.abspath(os.curdir)
+    tmp_path = tempfile.mkdtemp(suffix='_dbprocessing')
+    os.chdir(tmp_path)
+    
+    subprocess.check_call(' '.join(['/usr/bin/rsync ', '--dry-run ', '-auIv ',
+                                    os.path.join(data_path_inst, '*'),
+                                    miss_path_inst, ' > files.txt']),
+                          shell=True )
+    
+    with open('files.txt', 'r') as fp:
+        dat = fp.readlines()
+    DBlogging.dblogger.info('Copying {0} files to incoming for processing'.format(len(dat)-4)) # the 4 is for header and footer
+    for line in dat:
+        if '.ptp.gz' in line: # file we want to move
+            fname = os.path.join(data_path_inst, line.strip())
+            shutil.copy(fname, inc_path)
+            DBlogging.dblogger.debug('Copying {0} to incoming for processing'.format(fname))
+            
+    os.chdir(curdir)
+    shutil.rmtree(tmp_path)
 
-subprocess.check_call(' '.join(['/usr/bin/rsync ', '--dry-run ', '-auIv ',
-                                os.path.join(data_path_repta, '*'), miss_path_repta, ' > files.txt']), shell=True )
 
-with open('files.txt', 'r') as fp:
-    dat = fp.readlines()
-DBlogging.dblogger.info('Copying {0} files to incoming for processing'.format(len(dat)-5))
-for line in dat:
-    if '.ptp.gz' in line: # file we want to move
-        fname = os.path.join(data_path_repta, line.strip())
-        shutil.copy(fname, inc_path)
-        DBlogging.dblogger.debug('Copying {0} to incoming for processing'.format(fname))
+sats = ['a', 'b']
+insts = ['rept', 'mageis', 'hope']
 
-os.chdir(curdir)
-shutil.rmtree(tmp_path)
-
-
+for s, i in itertools.product(sats, insts):
+    sync_data(s, i)
+    #print s, i
 
 
