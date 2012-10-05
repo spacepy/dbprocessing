6478176087270538e107ebab23b98ec653d4418b
Brian Larsen <balarsen@lanl.gov>
2012-10-04 18:08:26 -0600

    dataToIncoming.py: also compares to error directory (used set()!!)

--- scripts/dataToIncoming.py
+++ scripts/dataToIncoming.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2.6
 
 import itertools
+import glob
 import os
 import tempfile
 import shutil
@@ -12,17 +13,20 @@ import dbprocessing.DBlogging as DBlogging
 # Code users rsync to build an incremental list of files not already
 # processed (i.e. not in /n/space_data/cda/rbsp) and saves that list to a file
 # then the file is opened and each file is copied using shutils.copy
+# also checks the error directory for the filename and does not copy again
 
 dbu = DBUtils.DBUtils('rbsp')
 
 mission_path = dbu.getMissionDirectory()
 inc_path = dbu.getIncomingPath()
 data_path = os.path.expanduser(os.path.join('~ectsoc', 'data', 'level_0'))
+error_path = dbu.getErrorPath()
 
 def sync_data(sc, inst):
     global mission_path
     global inc_path
     global data_path
+    global error_path
     data_path_inst = os.path.join(data_path, sc.lower(), inst.lower())
     miss_path_inst = os.path.join(mission_path, 'rbsp'+sc.lower(),
                                   inst, 'level0')
@@ -38,12 +42,18 @@ def sync_data(sc, inst):
     
     with open('files.txt', 'r') as fp:
         dat = fp.readlines()
-    DBlogging.dblogger.info('Copying {0} files to incoming for processing'.format(len(dat)-4)) # the 4 is for header and footer
+    dat = set([v.strip() for v in dat if '.ptp.gz' in v])
+    # get the files in incoming
+    dat_error = set([os.path.basename(v) for v in glob.glob(os.path.join(error_path, '*'))])
+    dat = dat.difference(dat_error)
+
+    if dat: # no need for a message if we ar movng nothing
+        DBlogging.dblogger.info('Copying {0} files to incoming for processing'.format(len(dat)-4)) # the 4 is for header and footer
+    
     for line in dat:
-        if '.ptp.gz' in line: # file we want to move
-            fname = os.path.join(data_path_inst, line.strip())
-            shutil.copy(fname, inc_path)
-            DBlogging.dblogger.debug('Copying {0} to incoming for processing'.format(fname))
+        fname = os.path.join(data_path_inst, line.strip())
+        shutil.copy(fname, inc_path)
+        DBlogging.dblogger.debug('Copying {0} to incoming for processing'.format(fname))
             
     os.chdir(curdir)
     shutil.rmtree(tmp_path)
