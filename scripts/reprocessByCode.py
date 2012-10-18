d29f9d3c59e89b9a5a022614111154cc72acb456
Brian Larsen <balarsen@lanl.gov>
2012-10-18 14:18:17 -0600

    reprocessByCode.py: updated scripts for new forcing for version

--- scripts/reprocessByCode.py
+++ scripts/reprocessByCode.py
@@ -30,8 +30,8 @@ if __name__ == "__main__":
                       help="Date to start reprocessing (e.g. 2012-10-02)", default=None)
     parser.add_option("-e", "--endDate", dest="endDate", type="string",
                       help="Date to end reprocessing (e.g. 2012-10-25)", default=None)
-    parser.add_option("", "--force", dest="force", action="store_true",
-                      help="Force the reprocessing", default=False)
+    parser.add_option("", "--force", dest="force", type="int",
+                      help="Force the reprocessing, speicify which version number {0},{1},{2}", default=None)
 
     (options, args) = parser.parse_args()
     if len(args) != 1:
@@ -47,7 +47,14 @@ if __name__ == "__main__":
         endDate = None
 
     db = dbprocessing.ProcessQueue('rbsp')
-    num = db.reprocessByCode(args[0], startDate=startDate, endDate=endDate, force=options.force)
+
+    if options.force is not None:
+        if options.force not in [0,1,2]:
+            parser.error("invalid force option [0,1,2]")
+        num = db.reprocessByCode(args[0], startDate=startDate, endDate=endDate, force=True, incVersion=options.force)
+    else:
+        num = db.reprocessByCode(args[0], startDate=startDate, endDate=endDate)
+
     print('Added {0} files to be reprocessed for code {1}'.format(num, args[0]))
     DBlogging.dblogger.info('Added {0} files to be reprocessed for code {1}'.format(num, args[0]))
 
