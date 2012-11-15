69e8539e103954363ed1503a679c3dbd8f0d6792
Brian Larsen <balarsen@lanl.gov>
2012-11-14 19:18:11 -0700

    DBlogging.py: try a manual rotation scheme

--- dbprocessing/DBlogging.py
+++ dbprocessing/DBlogging.py
@@ -1,4 +1,5 @@
 
+import datetime
 import logging
 import logging.handlers
 import os
@@ -13,9 +14,11 @@ Setup routines to log information from the dbprocessing chain
 
 """
 
+utctoday = datetime.datetime.utcnow().date().strftime('%Y-%m-%d')
 
 # TODO this should be setup by a config file
-LOG_FILENAME = os.path.expanduser(os.path.join('~','dbprocessing_logs', 'dbprocessing_log.log'))
+LOG_FILENAME = os.path.expanduser(os.path.join('~', 'dbprocessing_logs',
+                                               'dbprocessing_log.log.{0}'.format(utctoday)))
 
 # Set up a specific logger with our desired output level
 dblogger = logging.getLogger('DBLogger')
@@ -24,8 +27,9 @@ dblogger.setLevel(logging.DEBUG)
 # Add the log message handler to the logger
 #handler = logging.handlers.TimedRotatingFileHandler(
 #              LOG_FILENAME, maxBytes=20000000, backupCount=0) # keep them all
+## TODO this doesn't work so hardcode the name above, so break the rotation here
 handler = logging.handlers.TimedRotatingFileHandler(
-              LOG_FILENAME, when='midnight', interval=1, backupCount=0, utc=True) # keep them all
+              LOG_FILENAME, when='midnight', interval=1000000, backupCount=0, utc=True) # keep them all
 
 LEVELS = {'debug': logging.DEBUG,
           'info': logging.INFO,
