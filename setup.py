204c9b5fbe1906cad096de9fb66bc790681b4f23
Brian Larsen <balarsen@lanl.gov>
2013-06-07 17:16:25 -0600

    setup.py: added new script to install list

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ scripts = ('scripts/ProcessQueue.py', 'scripts/addProducts.py',
                'scripts/hope_dataToIncoming.py', 'scripts/reprocessByAll.py', 
 	       'scripts/hopeCoverageHTML.py', 'scripts/makeLatestSymlinks.py',
                'scripts/hope_query.py', 'scripts/magephem_dataToIncoming.py',
-               'scripts/magephem-pre-CoverageHTML.py')
+               'scripts/magephem-pre-CoverageHTML.py', 'scripts/magephem_def_dataToIncoming.py')
 
 scripts_dir = os.path.expanduser('~/dbUtils')
 
