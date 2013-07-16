50e66425e08651df8b642b69f482c078b25f79a4
Brian Larsen <balarsen@lanl.gov>
2013-07-16 13:27:20 -0600

    DBfile.py: properly performs substitusions before moving files to their locations

--- dbprocessing/test_DBfile.py
+++ dbprocessing/test_DBfile.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python2.6
 
+import datetime
 import unittest
 import os
+import tempfile
+
 
 import DBfile
 import DBUtils
 import Diskfile
 
+filename = 'test_file.txt'
 
-__version__ = '2.0.3'
+
+class dummyDBU(object):
+    def __init__(self):
+        self.mission = filename
 
 
 class DBfileTests(unittest.TestCase):
@@ -16,22 +23,33 @@ class DBfileTests(unittest.TestCase):
     
     def setUp(self):
         super(DBfileTests, self).setUp()
-        self.test_filename = 'Test-one_R0_evinst_20100112_v2.0.0.cdf'
-        with open(self.test_filename, 'wb') as f:
-            f.write('I am some text in a file')
-        self.dbu = DBUtils.DBUtils('Test')
-        self.df = Diskfile.Diskfile(self.test_filename, self.dbu)
-        #TODO: Diskfile appears to have never had this method
-        #self.df.parseAll()
+        with open(filename, 'w') as fp:
+            fp.write('I am some test data\n')
+        self.dbu = dummyDBU()
+        self.diskfile = Diskfile.Diskfile(filename, self.dbu)
+        self.diskfile.params['utc_file_date'] = datetime.date(2012, 4, 12)
+        self.diskfile.params['interface_version'] = 1 
+        self.diskfile.params['quality_version'] = 2
+        self.diskfile.params['revision_version'] = 3
+
 
     def tearDown(self):
         super(DBfileTests, self).tearDown()
-        os.remove(self.test_filename)
+        os.remove(filename)
 
-    def test_badInput(self):
-        """DBfile object will only take a Diskfile as input"""
-        self.assertRaises(DBfile.DBfileError, DBfile.DBfile,
-                          'wrong input', self.dbu)
+    def test_doDirSubs(self):
+        """the substitutions should work"""
+        dbf = DBfile.DBfile(self.diskfile, self.dbu)
+        path = '{Y}{m}{d}'
+        self.assertEqual('20120412', dbf._doDirSubs(path))
+        path = '{DATE}'
+        self.assertEqual('20120412', dbf._doDirSubs(path))
+        path = '{Y}{b}{d}'
+        self.assertEqual('2012Apr12', dbf._doDirSubs(path))
+        path = '{Y}{j}'
+        self.assertEqual('2012103', dbf._doDirSubs(path))
+        path = '{VERSION}'
+        self.assertEqual('1.2.3', dbf._doDirSubs(path))
 
 
 if __name__ == "__main__":
