8d482aff41e67fdd8fd19d3bef9951e179505078
Brian Larsen <balarsen@lanl.gov>
2013-07-17 13:50:45 -0600

    flushProcessQueue.py: fixed and generalized

--- scripts/flushProcessQueue.py
+++ scripts/flushProcessQueue.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python2.6
 
+import sys
+
 from dbprocessing import DBUtils
 
+if len(sys.argv) != 2:
+    print("Usage: {0} database".format(sys.argv[0]))
+    sys.exit(-1)
+
 if __name__ == "__main__":
-    a = DBUtils.DBUtils('rbsp')
-    a._openDB()
-    a._createTableObjects()
-    n_items = a.processqueueLen()
-    a.processqueueFlush()
+    a = DBUtils.DBUtils(sys.argv[1])
+    n_items = a.Processqueue.len()
+    a.Processqueue.flush()
     print 'Flushed {0} items from queue'.format(n_items)
 
