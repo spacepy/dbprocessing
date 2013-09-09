e8c4d240dfec4dd05745671274d3ffb0dc851b1e
RBSP ECT SOC <ectsoc@lanl.gov>
2013-09-09 12:00:12 -0600

    possibleProblemDates.py: added wrongNewestVersion()

--- scripts/possibleProblemDates.py
+++ scripts/possibleProblemDates.py
@@ -1,23 +1,64 @@
 #!/usr/bin/env python2.6
 
+import bisect
 import datetime
 import glob
+import itertools
 import os
 import sys
 import re
-import bisect
-import glob
 from optparse import OptionParser
-import os
 import re
 
 import dateutil.parser as dup
 import numpy as np
+from sqlalchemy import func
 
 import rbsp #rbsp.mission_day_to_UTC
 
-from dbprocessing import DBUtils, Utils, inspector
+from dbprocessing import DBUtils, Utils, Version, inspector
 from rbsp import Version
+
+from dbprocessing import DBlogging
+DBlogging.dblogger.setLevel(DBlogging.LEVELS['info'])
+
+def wrongNewestVersion(dbu, fix=False):
+    """
+    loop over products and dates and make sure the correct version is marked latest
+
+    do this by:
+    1) get all products
+    2) get all dates
+    3) loop over all products and dates (itertools.product)
+    4) get the versions for the files
+    5) find the largest
+    6) make sure it is marked newest and others are not
+    """
+    #firstDate = dbu.session.query(func.min(dbu.File.utc_file_date)).first()[0]
+    #lastDate  = dbu.session.query(func.max(dbu.File.utc_file_date)).first()[0]
+    #dates = Utils.expandDates(firstDate, lastDate)
+    prods = dbu.getAllProducts()
+
+    for p in prods:
+        files = dbu.getFilesByProduct(p.product_id, newest_version=False)
+        dates = sorted(list(set([v.utc_file_date for v in files])))
+
+        for date in dates:
+            ftmp = [v for v in files if v.utc_file_date == date]
+            versions = [dbu.getVersion(v) for v in ftmp]
+            mx = max(versions)
+            for f in ftmp:
+                if dbu.getVersion(f) != mx and f.newest_version:
+                    print('File {0}:{1} is marked newest, it should not be'.format(f.file_id, f.filename))
+                    if fix:
+                        f.newest_version = False
+                        dbu.session.add(f)
+                if dbu.getVersion(f) == mx and not f.newest_version:
+                    print('File {0}:{1} is not marked newest, it should be'.format(f.file_id, f.filename))
+                    if fix:
+                        f.newest_version = True
+                        dbu.session.add(f)
+            dbu._commitDB()
 
 
 
@@ -66,4 +107,7 @@ if __name__ == "__main__":
                 
     dbu = DBUtils.DBUtils(options.mission)
 
+    print("Running noNewestVersion()")
     noNewestVersion(dbu, options.fix)
+    print("Running wrongNewestVersion()")
+    wrongNewestVersion(dbu, options.fix)
