7878a351edb762d906892fe8b9a5c9d65e9e2766
Brian Larsen <balarsen@gmail.com>
2014-01-17 04:05:50 -0700

    test_Diskfile.py: removed obsollute tests

--- tests/test_Diskfile.py
+++ tests/test_Diskfile.py
@@ -2,20 +2,36 @@
 
 import datetime
 import os
+import shutil
 import stat
 import unittest
 
-import DBUtils
-import Diskfile
-import Version
+from dbprocessing import DBUtils
+from dbprocessing import Diskfile
+from dbprocessing import Version
 
 
-__version__ = '2.0.3'
+class TestSetup(unittest.TestCase):
+    """
+    master class for the setup and teardown
+    """
+    def setUp(self):
+        super(TestSetup, self).setUp()
+        sqpath = os.path.join(os.path.dirname(__file__), 'RBSP_MAGEIS.sqlite')
+        self.sqlworking = sqpath.replace('RBSP_MAGEIS.sqlite', 'working.sqlite')
+        shutil.copy(sqpath, self.sqlworking)
+        os.chmod(self.sqlworking, stat.S_IRUSR|stat.S_IWUSR)
+        self.dbu = DBUtils.DBUtils(self.sqlworking)
+
+    def tearDown(self):
+        super(TestSetup, self).tearDown()
+        self.dbu._closeDB()
+        del self.dbu
+        os.remove(self.sqlworking)
 
 
 class DiskfileStaticTests(unittest.TestCase):
     """Tests for the static methods in Diskfile"""
-
     def test_calcDigest(self):
         """ calcDigest  should behave correctly"""
         self.assertRaises(Diskfile.DigestError, Diskfile.calcDigest, 'idontexist.file')
@@ -32,83 +48,38 @@ class DiskfileStaticTests(unittest.TestCase):
         os.remove('IamAfileThatExists.file')
 
 
-class DiskfileTests(unittest.TestCase):
+class DiskfileTests(TestSetup):
     """Tests for Diskfile class"""
-
-    def setUp(self):
-        super(DiskfileTests, self).setUp()
-        dbo = DBUtils.DBUtils()
-        dbo._openDB()
-        dbo._createTableObjects()
-        self.dbo = dbo
-
-    def tearDown(self):
-        super(DiskfileTests, self).tearDown()
-        #Shouldn't this do something to close the db object created in setup?
-
     def test_read_error(self):
         """given a file input that is not readable raise ReadError:"""
         self.assertRaises(Diskfile.ReadError, Diskfile.Diskfile, 'wrong input',
-                          self.dbo)
-
-    def test_repr(self):
-        """repr retuens a known string"""
-        self.assertEqual("DBProcessing class instance for mission Test, version: 2.0.3", self.dbo.__repr__())
-
+                          self.dbu)
 
     def test_write_error(self):
         """given a file input that is not writeable WriteError"""
-        with open('IamAfileThatExists.file', 'wb') as f:
-            f.write('I am some text in a file')
-        os.chmod('IamAfileThatExists.file', stat.S_IRUSR)
-        self.assertRaises(Diskfile.WriteError, Diskfile.Diskfile,
-                          'IamAfileThatExists.file', self.dbo)
+        try:
+            with open('IamAfileThatExists.file', 'wb') as f:
+                f.write('I am some text in a file')
+            os.chmod('IamAfileThatExists.file', stat.S_IRUSR)
+            self.assertRaises(Diskfile.WriteError, Diskfile.Diskfile,
+                              'IamAfileThatExists.file', self.dbu)
 
-        os.chmod('IamAfileThatExists.file', stat.S_IWUSR|stat.S_IRUSR)
-        os.remove('IamAfileThatExists.file')
+            os.chmod('IamAfileThatExists.file', stat.S_IWUSR|stat.S_IRUSR)
+        finally:
+            try:
+                os.remove('IamAfileThatExists.file')
+            except OSError:
+                pass
 
     def test_init(self):
         """init does some checking"""
         with open('IamAfileThatExists.file', 'wb') as f:
             f.write('I am some text in a file')
         try:
-            a = Diskfile.Diskfile('IamAfileThatExists.file', self.dbo)
+            a = Diskfile.Diskfile('IamAfileThatExists.file', self.dbu)
         finally:
             os.remove('IamAfileThatExists.file')
 
-    def test_makeProductFilename(self):
-        """makeProductFilename shoould make a known filename"""
-        with open('Test-Test_R0_evinst_20090117_v1.0.0.cdf', 'wb') as f:
-            f.write('I am some text in a file')
-        try:
-            a = Diskfile.Diskfile('Test-Test_R0_evinst_20090117_v1.0.0.cdf',
-                                  self.dbo)
-            now = datetime.datetime.utcnow()
-            expect = u'Test-Test_L1_evinst_{:04}{:02}{:02}_v1.0.0.cdf'.format(
-                now.year, now.month, now.day)
-
-            self.assertEqual(a.makeProductFilename(17, datetime.datetime.utcnow(),
-                                    Version.Version(1, 0, 0)),
-                                    expect)
-        finally:
-            os.remove('Test-Test_R0_evinst_20090117_v1.0.0.cdf')
-
-    def test_makeProductFilenameChecks(self):
-        """makeProductFilename should do some checking"""
-        with open('Test-Test_R0_evinst_20090117_v1.0.0.cdf', 'wb') as f:
-            f.write('I am some text in a file')
-        try:
-            a = Diskfile.Diskfile('Test-Test_R0_evinst_20090117_v1.0.0.cdf',
-                                  self.dbo)
-            self.assertRaises(Diskfile.InputError, a.makeProductFilename, 17,
-                              datetime.datetime.utcnow(), '1.0.0')
-            self.assertRaises(Diskfile.InputError, a.makeProductFilename, 17,
-                              'bad in', Version.Version(1, 0, 0))
-            self.assertRaises(Diskfile.InputError, a.makeProductFilename, 17,
-                              datetime.datetime.utcnow(), Version.Version(1,0,0),
-                              'bad in')
-        finally:
-            os.remove('Test-Test_R0_evinst_20090117_v1.0.0.cdf')
 
 
 if __name__ == "__main__":
