05f61ae17f747c636c3d592891e687b0be7ad216
RBSP ECT SOC <ectsoc@lanl.gov>
2014-03-17 15:50:35 -0600

    magephem_def_dataToIncoming.py: update for the new db

--- scripts/magephem_def_dataToIncoming.py
+++ scripts/magephem_def_dataToIncoming.py
@@ -30,12 +30,12 @@ def perdelta(start, end, delta):
         curr += delta
 
 
-dbu = DBUtils.DBUtils(os.path.expanduser('~ectsoc/MagEphem_def_processing.sqlite'))
+dbu = DBUtils.DBUtils(os.path.expanduser('~ectsoc/RBSP_MAGEPHEM_def.sqlite'))
 
 mission_path = dbu.getMissionDirectory()
 g_inc_path = dbu.getIncomingPath()
 
-files = dbu.getFilesByProduct('rbsp_def_kernel')
+files = dbu.getFilesByProduct('rbspa-def_kernel')
 #files = sorted(files, key=lambda x: x.utc_file_date)[-1]
 dbu._closeDB()
 
@@ -59,7 +59,7 @@ os.remove(tmpfile.name)
 
 allfiles = []
 for result in perdelta(datetime.date(2012, 8, 30), enddate, datetime.timedelta(days=1)):
-    allfiles.append('Setup_{0}.ker'.format(result.strftime('%Y%m%d')))
+    allfiles.append('rbspa_Setup_{0}.ker'.format(result.strftime('%Y%m%d')))
 
 allfiles = set(allfiles)
 
@@ -73,41 +73,86 @@ for f in files_to_make:
 
 
 
-##################################################################
-# Also need to do the same thing making QinDenton inputs
-##################################################################
-dbu = DBUtils.DBUtils(os.path.expanduser('~ectsoc/MagEphem_def_processing.sqlite'))
+####################################################################################################
+
+
+dbu = DBUtils.DBUtils(os.path.expanduser('~ectsoc/RBSP_MAGEPHEM_def.sqlite'))
 
 mission_path = dbu.getMissionDirectory()
 g_inc_path = dbu.getIncomingPath()
 
-files = dbu.getFilesByProduct('QinDenton_Virbo')
-Virbo_dbfiles = set(v.filename for v in files)
-
-files = dbu.getFilesByProduct('QinDenton_NRT')
-NRT_dbfiles = set(v.filename for v in files)
-
+files = dbu.getFilesByProduct('rbspb-def_kernel')
+#files = sorted(files, key=lambda x: x.utc_file_date)[-1]
 dbu._closeDB()
 
+dbfiles = set(v.filename for v in files)
 
-Virbo_files = glob.glob('/n/space_data/MagModelInputs/QinDenton/201[23456789]/QinDenton_*_1min.txt')
-Virbo_files = set(['Virbo_' + os.path.basename(v) for v in Virbo_files])
+# make a metakernel and get the end date from it
+tmpfile = tempfile.NamedTemporaryFile(delete=False, prefix='magephem_def')
+tmpfile.close()
+cmd = [os.path.expanduser('~/.local/bin/newMetaKernel.py'), '-d', tmpfile.name]
+print(' '.join(cmd))
+subprocess.check_call(cmd, shell=False)
+naif.spice.load_kern(tmpfile.name)
+kern_dates = []
+for i in range(naif.spice.n_kernels()):
+    try:
+        kern_dates.append(naif.spice.kernel_coverage(i).values()[0][0][1])
+    except TypeError:
+        pass
+enddate = min(kern_dates).date()
+os.remove(tmpfile.name)
 
-NRT_files = glob.glob('/n/space_data/MagModelInputs/nrtQinDenton/201[23456789]/QinDenton_*_1min.txt')
-NRT_files = set(['NRT_' + os.path.basename(v) for v in NRT_files])
+allfiles = []
+for result in perdelta(datetime.date(2012, 8, 30), enddate, datetime.timedelta(days=1)):
+    allfiles.append('rbspb_Setup_{0}.ker'.format(result.strftime('%Y%m%d')))
 
-files_to_make_virbo = Virbo_files.difference(Virbo_dbfiles)
-files_to_make_nrt   = NRT_files.difference(NRT_dbfiles)
+allfiles = set(allfiles)
 
-for f in files_to_make_virbo:
-    cmd = ['touch',  os.path.join(g_inc_path, "{0}".format(f))]
+files_to_make = allfiles.difference(dbfiles)
+
+for f in files_to_make:
+    cmd = [os.path.expanduser('~/.local/bin/newMetaKernel.py'), '-d',
+           os.path.join(g_inc_path, f)]
     print(' '.join(cmd))
     subprocess.check_call(cmd, shell=False)
+    
 
-for f in files_to_make_nrt:
-    cmd = ['touch',  os.path.join(g_inc_path, "{0}".format(f))]
-    print(' '.join(cmd))
-    subprocess.check_call(cmd, shell=False)
+##################################################################
+## # Also need to do the same thing making QinDenton inputs
+## ##################################################################
+## dbu = DBUtils.DBUtils(os.path.expanduser('~ectsoc/MagEphem_def_processing.sqlite'))
+
+## mission_path = dbu.getMissionDirectory()
+## g_inc_path = dbu.getIncomingPath()
+
+## files = dbu.getFilesByProduct('QinDenton_Virbo')
+## Virbo_dbfiles = set(v.filename for v in files)
+
+## files = dbu.getFilesByProduct('QinDenton_NRT')
+## NRT_dbfiles = set(v.filename for v in files)
+
+## dbu._closeDB()
+
+
+## Virbo_files = glob.glob('/n/space_data/MagModelInputs/QinDenton/201[23456789]/QinDenton_*_1min.txt')
+## Virbo_files = set(['Virbo_' + os.path.basename(v) for v in Virbo_files])
+
+## NRT_files = glob.glob('/n/space_data/MagModelInputs/nrtQinDenton/201[23456789]/QinDenton_*_1min.txt')
+## NRT_files = set(['NRT_' + os.path.basename(v) for v in NRT_files])
+
+## files_to_make_virbo = Virbo_files.difference(Virbo_dbfiles)
+## files_to_make_nrt   = NRT_files.difference(NRT_dbfiles)
+
+## for f in files_to_make_virbo:
+##     cmd = ['touch',  os.path.join(g_inc_path, "{0}".format(f))]
+##     print(' '.join(cmd))
+##     subprocess.check_call(cmd, shell=False)
+
+## for f in files_to_make_nrt:
+##     cmd = ['touch',  os.path.join(g_inc_path, "{0}".format(f))]
+##     print(' '.join(cmd))
+##     subprocess.check_call(cmd, shell=False)
 
 
 
