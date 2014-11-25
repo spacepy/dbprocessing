f99fd8519c9c10e06775154f1b00b45c619bdcdc
Brian Larsen <balarsen@lanl.gov>
2014-11-25 15:30:00 -0700

    scripts: added purgeFileFromDB.py to remove files form a db, not recursiove yet

---
+++ scripts/purgeFileFromDB.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python2.6
+
+from __future__ import print_function
+
+import itertools
+import glob
+import os
+from optparse import OptionParser
+import re
+import sys
+import traceback
+import warnings
+
+from spacepy import pycdf
+
+from dbprocessing import DBUtils
+from dbprocessing import Version
+
+
+if __name__ == '__main__':
+    usage = "usage: %prog -m database filename [filename [filename] ...]"
+    parser = OptionParser(usage=usage)
+    parser.add_option("-r", "--recursive", dest="recursive", action="store_true",
+                      help="Recursive removal", default=False)
+    parser.add_option("-m", "--mission", dest="mission",
+                      help="selected mission database", default=None)
+    (options, args) = parser.parse_args()
+
+    if len(args) < 1:
+        parser.error("incorrect number of arguments")
+
+    if options.recursive:
+        raise(NotImplementedError("Recursive removal not implemented in DBUtils yet"))
+        
+    dbu = DBUtils.DBUtils(options.mission)
+
+    for f in args:
+        try:
+            f_id = dbu.getFileID(f)
+        except DBUtils.DBNoData:
+            print("WARNING: File {1} not in db".format(f), file=sys.stderr)
+        else:
+            dbu._purgeFileFromDB(f)
+            print("  File {0}:{1} removed from DB".format(f_id, f))
+
+    
+    dbu._closeDB()
+        
