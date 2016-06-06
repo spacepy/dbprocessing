8026744693feb907381f86ba52e06730d71463c9
Myles Johnson <Myles.s.Johnson@gmail.com>
2016-06-06 09:32:47 -0400

    Modified functionalTest.sh to always regen the db(It's not going to be run often enough to justify the possible confusion)

--- test_DB/scripts/functionalTest.sh
+++ test_DB/scripts/functionalTest.sh
@@ -1,10 +1,5 @@
-#If this is first run, make the starting DB
-if [ ! -f ../testDB.sqlite.bak ]; then
-	./CreateDB.py ../testDB.sqlite.bak
-	./addFromConfig.py -m ../testDB.sqlite.bak ../config/testDB.conf
-fi
-
-#Copy back the starting DB and rerun the injest and process
-cp ../testDB.sqlite.bak ../testDB.sqlite
+rm ../testDB.sqlite
+./CreateDB.py ../testDB.sqlite
+./addFromConfig.py -m ../testDB.sqlite ../config/testDB.conf
 ./ProcessQueue.py -i -m ../testDB.sqlite
 ./ProcessQueue.py -p -m ../testDB.sqlite
