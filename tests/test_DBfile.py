2f7e4da58aaf9348d1debac243e4fdac7788e6bd
Myles Johnson <Myles.s.Johnson@gmail.com>
2016-06-06 15:15:09 -0400

    Added more DBfile tests

--- tests/test_DBfile.py
+++ tests/test_DBfile.py
@@ -32,6 +32,21 @@ class DBfileTests(unittest.TestCase):
         super(DBfileTests, self).tearDown()
         remove_tree(self.tempD)
 
+    def createDummyDBF(self, fname):
+        dbf = DBfile.DBfile(self.tempD + fname, self.dbu, makeDiskFile=True)
+        dbf.diskfile.params['utc_file_date'] = datetime.date.today()
+        dbf.diskfile.params['utc_start_time'] = datetime.date.today()
+        dbf.diskfile.params['utc_stop_time'] = datetime.date.today() + datetime.timedelta(days=1)
+        dbf.diskfile.params['data_level'] = 0
+        dbf.diskfile.params['file_create_date'] = datetime.date.today()
+        dbf.diskfile.params['exists_on_disk'] = 1
+        dbf.diskfile.params['product_id'] = 1
+        dbf.diskfile.params['shasum'] = Diskfile.calcDigest(self.tempD + fname)
+        dbf.diskfile.params['version'] = Version.Version(1, 2, 3)
+        dbf.diskfile.params['newest_version'] = 1
+
+        return dbf
+
     def test_invalidInput(self):
         self.assertRaises(DBfile.DBfileError, DBfile.DBfile, self.tempD + '/L0/testDB_000_first.raw', self.dbu)
 
@@ -41,7 +56,8 @@ class DBfileTests(unittest.TestCase):
 
     def test_getDirectory(self):
         dbf = DBfile.DBfile(self.tempD + '/L0/testDB_000_first.raw', self.dbu, makeDiskFile=True)
-        #self.assertRaises(DBfile.DBfileError, dbf.getDirectory() )
+        
+        self.assertRaises(DBfile.DBfileError, dbf.getDirectory )
 
         dbf.diskfile.params['product_id'] = 4
         self.assertEqual( self.tempD + '/L0', dbf.getDirectory() )
@@ -49,23 +65,40 @@ class DBfileTests(unittest.TestCase):
     def test_addFileToDB(self):
         with open(self.tempD + '/file.file', 'w') as fp:
             fp.write('I am some test data\n')
-
-        dbf = DBfile.DBfile(self.tempD + '/file.file', self.dbu, makeDiskFile=True)
-        dbf.diskfile.params['utc_file_date'] = datetime.date.today()
-        dbf.diskfile.params['utc_start_time'] = datetime.date.today()
-        dbf.diskfile.params['utc_stop_time'] = datetime.date.today() + datetime.timedelta(days=1)
-        dbf.diskfile.params['data_level'] = 0
-        dbf.diskfile.params['file_create_date'] = datetime.date.today()
-        dbf.diskfile.params['exists_on_disk'] = 1
-        dbf.diskfile.params['product_id'] = 1
-        dbf.diskfile.params['shasum'] = Diskfile.calcDigest(self.tempD + '/file.file')
-        dbf.diskfile.params['version'] = Version.Version(1, 2, 3)
-        dbf.diskfile.params['newest_version'] = 1
+        dbf = self.createDummyDBF('/file.file')
 
         self.assertTrue(dbf.addFileToDB())
 
-    def test_move(self):
-        pass
+    def test_move1(self):
+        with open(self.tempD + '/file.file', 'w') as fp:
+            fp.write('I am some test data\n')
+        dbf = self.createDummyDBF('/file.file')
+
+        real_ans = (self.tempD + '/file.file', self.tempD + '/L1/file.file')
+        self.assertEqual(real_ans, dbf.move())
+
+    def test_move2(self):
+        with open(self.tempD + '/file.file', 'w') as fp:
+            fp.write('I am some test data\n')
+        os.symlink(self.tempD + '/file.file', self.tempD + '/sym.file')
+        dbf = self.createDummyDBF('/sym.file')
+
+        real_ans = (self.tempD + '/sym.file', self.tempD + '/L1/sym.file')
+        self.assertTrue(os.path.isfile(self.tempD + '/sym.file'))
+        self.assertEqual(real_ans, dbf.move())
+        self.assertFalse(os.path.isfile(self.tempD + '/sym.file'))
+
+    def test_move3(self):
+        with open(self.tempD + '/file.file', 'w') as fp:
+            fp.write('I am some test data\n')
+        dbf = self.createDummyDBF('/file.file')
+
+        remove_tree(self.tempD+'/L1')
+
+        real_ans = (self.tempD + '/file.file', self.tempD + '/L1/file.file')
+        self.assertFalse(os.path.isdir(self.tempD + '/L1'))
+        self.assertEqual(real_ans, dbf.move())
+        self.assertTrue(os.path.isdir(self.tempD + '/L1'))
 
 if __name__ == "__main__":
     unittest.main()
