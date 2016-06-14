f1e78764a66fbb60a1b20692bc548e9d236c6e5f
Myles Johnson <Myles.s.Johnson@gmail.com>
2016-06-14 12:22:45 -0400

    Rewrote a fair amount of Version.py

--- dbprocessing/Version.py
+++ dbprocessing/Version.py
@@ -1,23 +1,6 @@
 from __future__ import division
 from __future__ import print_function
 
-"""
-Module to handle version information of files and codes.
-
-Handles Boolean operators (>, <, =, !=) and database interface for version is also implemented in another module
-
-The version scheme is X,Y,Z where:
-* X is the interface version, incremented only when the interface to a file or code changes
-* Y is the quality version, incremented when a change is made to a file that affects quality
-* Z is the revision version, incremented when a revision has been done to a code or file, as minor as fixing a typo
-
-Note
-====
-The interface version starts at 1
-
-"""
-
-
 class VersionError(Exception):
     """Error class for Version that calls out that an invalid version has been specified"""
     pass
@@ -26,30 +9,23 @@ class VersionError(Exception):
 class Version(object):
     """
     A version class to simplify pushing around version information
+    Handles Boolean operators (>, <, =, !=) and database interface for version is also implemented in another module
 
-    `Author:` Brian Larsen, LANL
+    The version scheme is X,Y,Z where:
+    * X is the interface version, incremented only when the interface to a file or code changes
+    * Y is the quality version, incremented when a change is made to a file that affects quality
+    * Z is the revision version, incremented when a revision has been done to a code or file, as minor as fixing a typo
 
-    Parameters
-    ==========
-    interface_version : int
-        the interface version for the object
-    quality_version : int
-        the quality version for the object
-    revision_version : int
-        the revision version of the object
+    .. note:: The interface version starts at 1
 
-    Attributes
-    ==========
-    interface : int
-        interface version of the object
-    quality : int
-        quality version of the object
-    revision : int
-        revision version of the object
+    .. Author:: Brian Larsen, LANL
 
+    :attribute interface: The interface version for the object
+    :attribute quality: The quality version for the object
+    :attribute revision: The revision version for the object
 
-    Examples
-    ========
+    :Example:
+
     >>> import Version
     >>> v = Version.Version(1,1,1)
     >>> print(v)
@@ -73,10 +49,17 @@ class Version(object):
     True
     """
 
-    def __init__(self,
-                 interface_version,
-                 quality_version,
-                 revision_version):
+    def __init__(self, interface_version, quality_version, revision_version):
+        """
+        Sets the class attributes
+
+        :param interface_version: The interface version for the object
+        :type interface_version: int
+        :param quality_version: The quality version for the object
+        :type quality_version: int
+        :param revision_version: The revision version for the object
+        :type revision_version: int
+        """
         self.interface = int(interface_version)
         self.revision = int(revision_version)
         self.quality = int(quality_version)
@@ -85,40 +68,41 @@ class Version(object):
     @staticmethod
     def fromString(inval):
         """
-        given a string of the form x.y.z return a Version object
+        Given a string of the form x.y.z return a Version object
+        :param inval: String in the form x.y.z 
+        :type inval: str
         """
         return Version(*inval.split('.'))
 
     def _checkVersion(self):
         """
-        check a version to make sure it is valid, works on current object
+        Check a version to make sure it is valid, works on current object
         """
         if self.interface == 0:
             raise (VersionError("interface_version starts at 1"))
 
     def __repr__(self):
-        return 'Version: ' + str(self.interface) + '.' + str(self.quality) + '.' + \
-               str(self.revision)
+        return 'Version: ' + self.__str__()
 
     def __str__(self):
         return str(self.interface) + '.' + str(self.quality) + '.' + \
                str(self.revision)
 
     def incInterface(self):
-        """increment the interface version and reset the other two"""
+        """Increment the interface version and reset the other two"""
         self.interface += 1
         self.quality = 0
         self.revision = 0
         self._checkVersion()
 
     def incQuality(self):
-        """increment the quality version and reset the revision"""
+        """Increment the quality version and reset the revision"""
         self.quality += 1
         self.revision = 0
         self._checkVersion()
 
     def incRevision(self):
-        """increment the revision version"""
+        """Increment the revision version"""
         self.revision += 1
         self._checkVersion()
 
@@ -130,37 +114,23 @@ class Version(object):
                     return True
         return False
 
+
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
     def __gt__(self, other):
         val_s = 10000 * self.interface + 100 * self.quality + self.revision
         val_o = 10000 * other.interface + 100 * other.quality + other.revision
-        if val_s > val_o:
-            return True
-        else:
-            return False
+        return val_s > val_o
 
     def __lt__(self, other):
-        val_s = 10000 * self.interface + 100 * self.quality + self.revision
-        val_o = 10000 * other.interface + 100 * other.quality + other.revision
-        if val_s < val_o:
-            return True
-        else:
-            return False
+        return not self.__gt__(other) and not self.__eq__(other)
 
     def __le__(self, other):
         return self.__eq__(other) or self.__lt__(other)
 
     def __ge__(self, other):
         return self.__eq__(other) or self.__gt__(other)
-
-    def __ne__(self, other):
-        if self.interface != other.interface:
-            return True
-        elif self.quality != other.quality:
-            return True
-        elif self.revision != other.revision:
-            return True
-        else:
-            return False
 
     def __sub__(self, other):
         return [self.interface - other.interface, self.quality - other.quality, self.revision - other.revision]
--- sphinx/source/Version.rst
+++ sphinx/source/Version.rst
@@ -1,13 +1,12 @@
 Version
 =======
-Module to handle version information of files and codes.
+.. automodule:: dbprocessing.Version
 
-Handles boolean operators (>, <, =, !=) and database interface for version is also implemented in another module
+.. currentmodule:: dbprocessing.Version
 
-The version scheme is X,Y,Z where:
-* X is the interface version, incremented only when the interface to a file or code changes
-* Y is the quality version, incremented when a change is made to a file that affects quality
-* Z is the revision version, incremented when a revision has been done to a code or file, as minor as fixing a typo
+.. rubric:: Classes
 
-`Note:` The interface version starts at 1
+.. autosummary::
+    :toctree: autosummary
 
+    Version
