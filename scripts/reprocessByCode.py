c040ed7cc304c133cc75de165ad176e0a5b9603c
Myles Johnson <Myles.s.Johnson@gmail.com>
2016-06-17 16:56:50 -0400

    Make reprocesByCode.py take the mission as a option instead of being hardcoded

--- scripts/reprocessByCode.py
+++ scripts/reprocessByCode.py
@@ -32,6 +32,8 @@ if __name__ == "__main__":
                       help="Date to end reprocessing (e.g. 2012-10-25)", default=None)
     parser.add_option("", "--force", dest="force", type="int",
                       help="Force the reprocessing, speicify which version number {0},{1},{2}", default=None)
+    parser.add_option("-m", "--mission", dest="mission",
+                      help="selected mission database", default=None)
 
     (options, args) = parser.parse_args()
     if len(args) != 1:
@@ -46,7 +48,7 @@ if __name__ == "__main__":
     else:
         endDate = None
 
-    db = dbprocessing.ProcessQueue('rbsp')
+    db = dbprocessing.ProcessQueue(options.mission)
 
     if options.force not in [None, 0, 1, 2]:
         parser.error("invalid force option [0,1,2]")
