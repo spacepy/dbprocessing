83c273263614d101ba4234e0b3765ea66a48f164
Ken Fairchild <fair-play@comcast.net>
2016-06-21 10:45:06 -0400

    Initial files to support inspector class testing

---
+++ tests/inspector/rot13_L1.py
@@ -0,0 +1,20 @@
+from dbprocessing import inspector
+from dbprocessing import Version
+
+import re
+import datetime
+
+class Inspector(inspector.inspector):
+	code_name = "rot13_L1.py"
+
+	def inspect(self, kwargs):
+		m = re.match(r'testDB_(\d{3}).*\.raw$', self.basename)
+		if not m:
+			return None
+
+		self.diskfile.params['utc_start_time'] = datetime.datetime(2016, 1, 1) + datetime.timedelta(days=int(m.group(1)))
+		self.diskfile.params['utc_stop_time'] = self.diskfile.params['utc_start_time'] + datetime.timedelta(days=1)
+		self.diskfile.params['utc_file_date'] = self.diskfile.params['utc_start_time'].date()
+		self.diskfile.params['version'] = Version.Version(1, 0, 0)
+		self.diskfile.params['process_keywords'] = 'nnn=' + m.group(1)
+		return True
