cdbd4e1575702f8812029a20d0e74fccae4466ac
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2017-02-21 14:56:18 -0500

    Revert "Update sphinx config" (still have old sphinx elsewhere)
    This reverts commit 5e8780c08ab06bb04cbba6bccf349e8699276250.

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -68,7 +68,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'dbprocessing'
-copyright = u'2017, Brian Larsen, Jonathan Niehof'
+copyright = u'2016, Brian Larsen, Jonathan Niehof'
 author = u'Brian Larsen, Jonathan Niehof'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -130,7 +130,7 @@ todo_include_todos = True
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+html_theme = 'default'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -307,7 +307,298 @@ texinfo_documents = [
 
 
 # Example configuration for intersphinx: refer to the Python standard library.
-intersphinx_mapping = {'https://docs.python.org/2.7/': None}
+intersphinx_mapping = {'https://docs.python.org/': None}
+
+#Fix for https://github.com/sphinx-doc/sphinx/issues/1585
+#Have to monkey-patch in the entire method for a one-line fix.
+#This is essentially verbatim from 1.2.3, plus the fix, and some namespacing
+#to keep from bringing in all of sphinx.ext.autosummary
+import sphinx
+if sphinx.__version__ == '1.2.3':
+    import sphinx.ext.autosummary
+    import re
+    from types import ModuleType
+    from docutils.statemachine import ViewList
+    from sphinx.pycode import ModuleAnalyzer, PycodeError
+    class FixedAutosummary(sphinx.ext.autosummary.Autosummary):
+        def get_items(self, names):
+            """Try to import the given names, and return a list of
+            ``[(name, signature, summary_string, real_name), ...]``.
+            """
+            env = self.state.document.settings.env
+
+            prefixes = sphinx.ext.autosummary.get_import_prefixes_from_env(env)
+
+            items = []
+
+            max_item_chars = 50
+
+            for name in names:
+                display_name = name
+                if name.startswith('~'):
+                    name = name[1:]
+                    display_name = name.split('.')[-1]
+
+                try:
+                    real_name, obj, parent, modname = sphinx.ext.autosummary.import_by_name(name, prefixes=prefixes)
+                except ImportError:
+                    self.warn('failed to import %s' % name)
+                    items.append((name, '', '', name))
+                    continue
+
+                self.result = ViewList()  # initialize for each documenter
+                full_name = real_name
+                if not isinstance(obj, ModuleType):
+                    # give explicitly separated module name, so that members
+                    # of inner classes can be documented
+                    full_name = modname + '::' + full_name[len(modname)+1:]
+                # NB. using full_name here is important, since Documenters
+                #     handle module prefixes slightly differently
+                documenter = sphinx.ext.autosummary.get_documenter(obj, parent)(self, full_name)
+                if not documenter.parse_name():
+                    self.warn('failed to parse name %s' % real_name)
+                    items.append((display_name, '', '', real_name))
+                    continue
+                if not documenter.import_object():
+                    self.warn('failed to import object %s' % real_name)
+                    items.append((display_name, '', '', real_name))
+                    continue
+
+                # try to also get a source code analyzer for attribute docs
+                try:
+                    documenter.analyzer = ModuleAnalyzer.for_module(
+                        documenter.get_real_modname())
+                    # parse right now, to get PycodeErrors on parsing (results will
+                    # be cached anyway)
+                    documenter.analyzer.find_attr_docs()
+                except PycodeError, err:
+                    documenter.env.app.debug(
+                        '[autodoc] module analyzer failed: %s', err)
+                    # no source file -- e.g. for builtin and C modules
+                    documenter.analyzer = None
+
+                # -- Grab the signature
+
+                sig = documenter.format_signature()
+                if not sig:
+                    sig = ''
+                else:
+                    max_chars = max(10, max_item_chars - len(display_name))
+                    sig = sphinx.ext.autosummary.mangle_signature(sig, max_chars=max_chars)
+                    sig = sig.replace('*', r'\*')
+
+                # -- Grab the summary
+
+                documenter.add_content(None)
+                doc = list(documenter.process_doc([self.result.data]))
+
+                while doc and not doc[0].strip():
+                    doc.pop(0)
+
+                # If there's a blank line, then we can assume the first sentence /
+                # paragraph has ended, so anything after shouldn't be part of the
+                # summary
+                for i, piece in enumerate(doc):
+                    if not piece.strip():
+                        doc = doc[:i]
+                        break
+
+                # Try to find the "first sentence", which may span multiple lines
+                m = re.search(r"^([A-Z].*?\.)(?:\s|$)", " ".join(doc).strip())
+                if m:
+                    summary = m.group(1).strip()
+                elif doc:
+                    summary = doc[0].strip()
+                else:
+                    summary = ''
+
+                items.append((display_name, sig, summary, real_name))
+
+            return items
+    sphinx.ext.autosummary.Autosummary = FixedAutosummary
+
+if sphinx.__version__ == '1.2.2':
+    #Monkey-patch to put data items in automodule
+    #This is essentially verbatim from 1.2.3, new code is commented with ##
+    from jinja2 import FileSystemLoader, TemplateNotFound
+    from jinja2.sandbox import SandboxedEnvironment
+
+    from sphinx import package_dir
+    from sphinx.ext.autosummary import import_by_name, get_documenter
+    from sphinx.jinja2glue import BuiltinTemplateLoader
+    from sphinx.pycode import ModuleAnalyzer
+    from sphinx.util.osutil import ensuredir
+    from sphinx.util.inspect import safe_getattr
+
+    import sphinx.ext.autosummary.generate
+    from sphinx.ext.autosummary.generate import _simple_warn, _simple_info, \
+        find_autosummary_in_files
+    def generate_autosummary_docs(sources, output_dir=None, suffix='.rst',
+                                  warn=_simple_warn, info=_simple_info,
+                                  base_path=None, builder=None, template_dir=None):
+        showed_sources = list(sorted(sources))
+        if len(showed_sources) > 20:
+            showed_sources = showed_sources[:10] + ['...'] + showed_sources[-10:]
+        info('[autosummary] generating autosummary for: %s' %
+             ', '.join(showed_sources))
+
+        if output_dir:
+            info('[autosummary] writing to %s' % output_dir)
+
+        if base_path is not None:
+            sources = [os.path.join(base_path, filename) for filename in sources]
+
+        # create our own templating environment
+        template_dirs = [os.path.join(package_dir, 'ext',
+                                      'autosummary', 'templates')]
+        if builder is not None:
+            # allow the user to override the templates
+            template_loader = BuiltinTemplateLoader()
+            template_loader.init(builder, dirs=template_dirs)
+        else:
+            if template_dir:
+                template_dirs.insert(0, template_dir)
+            template_loader = FileSystemLoader(template_dirs)
+        template_env = SandboxedEnvironment(loader=template_loader)
+
+        # read
+        items = find_autosummary_in_files(sources)
+
+        # remove possible duplicates
+        items = dict([(item, True) for item in items]).keys()
+
+        # keep track of new files
+        new_files = []
+
+        # write
+        for name, path, template_name in sorted(items, key=str):
+            if path is None:
+                # The corresponding autosummary:: directive did not have
+                # a :toctree: option
+                continue
+
+            path = output_dir or os.path.abspath(path)
+            ensuredir(path)
+
+            try:
+                res = import_by_name(name)
+                if len(res) == 3:
+                    name, obj, parent = res
+                else:
+                    name, obj, parent, mod_name = res
+            except ImportError, e:
+                warn('[autosummary] failed to import %r: %s' % (name, e))
+                continue
+
+            fn = os.path.join(path, name + suffix)
+
+            # skip it if it exists
+            if os.path.isfile(fn):
+                continue
+
+            new_files.append(fn)
+
+            f = open(fn, 'w')
+
+            try:
+                doc = get_documenter(obj, parent)
+
+                if template_name is not None:
+                    template = template_env.get_template(template_name)
+                else:
+                    try:
+                        template = template_env.get_template('autosummary/%s.rst'
+                                                             % doc.objtype)
+                    except TemplateNotFound:
+                        template = template_env.get_template('autosummary/base.rst')
+
+                def get_members(obj, typ, include_public=[]):
+                    items = []
+                    for name in dir(obj):
+                        try:
+                            documenter = get_documenter(safe_getattr(obj, name),
+                                                        obj)
+                        except AttributeError:
+                            continue
+                        if documenter.objtype == typ:
+                            items.append(name)
+                    public = [x for x in items
+                              if x in include_public or not x.startswith('_')]
+                    return public, items
+
+                ns = {}
+
+                if doc.objtype == 'module':
+                    ns['members'] = dir(obj)
+                    ns['functions'], ns['all_functions'] = \
+                                       get_members(obj, 'function')
+                    ns['classes'], ns['all_classes'] = \
+                                     get_members(obj, 'class')
+                    ns['exceptions'], ns['all_exceptions'] = \
+                                       get_members(obj, 'exception')
+                    ##Added, following four lines
+                    ns['data'], ns['all_data'] = \
+                                       get_members(obj, 'data')
+                    ns['attributes'], ns['all_attributes'] = \
+                                     get_members(obj, 'attribute')
+                elif doc.objtype == 'class':
+                    ns['members'] = dir(obj)
+                    ns['methods'], ns['all_methods'] = \
+                                     get_members(obj, 'method', ['__init__'])
+                    ns['attributes'], ns['all_attributes'] = \
+                                     get_members(obj, 'attribute')
+                    ##NEW
+                    #Try to get stuff that's only in attributes
+                    if hasattr(obj, '__module__'):
+                        realmodule = obj.__module__
+                    elif hasattr(parent, '__module__'):
+                        realmodule = parent.__module__
+                    else:
+                        realmodule = None
+                    if realmodule:
+                        #Keyed by a tuple of (class name, attribute name)
+                        #Result is a list of docstrings
+                        docattrs = ModuleAnalyzer.for_module(
+                            realmodule).find_attr_docs()
+                        moreattrs = [k[1] for k in docattrs.keys()
+                                     if k[1] not in ns['all_attributes']
+                                     and k[0] == obj.__name__]
+                        ns['all_attributes'].extend(moreattrs)
+                        ns['attributes'].extend(
+                            [a for a in moreattrs if not a.startswith('_')])
+                    ##END NEW
+
+                parts = name.split('.')
+                if doc.objtype in ('method', 'attribute'):
+                    mod_name = '.'.join(parts[:-2])
+                    cls_name = parts[-2]
+                    obj_name = '.'.join(parts[-2:])
+                    ns['class'] = cls_name
+                else:
+                    mod_name, obj_name = '.'.join(parts[:-1]), parts[-1]
+
+                ns['fullname'] = name
+                ns['module'] = mod_name
+                ns['objname'] = obj_name
+                ns['name'] = parts[-1]
+
+                ns['objtype'] = doc.objtype
+                ns['underline'] = len(name) * '='
+
+                rendered = template.render(**ns)
+                f.write(rendered)
+            finally:
+                f.close()
+
+        # descend recursively to new files
+        if new_files:
+            generate_autosummary_docs(new_files, output_dir=output_dir,
+                                      suffix=suffix, warn=warn, info=info,
+                                      base_path=base_path, builder=builder,
+                                      template_dir=template_dir)
+
+    sphinx.ext.autosummary.generate.generate_autosummary_docs = \
+        generate_autosummary_docs
 
 #If the autosummary on data has docstring of object constructor instead
 #of the docstring attached to the object, look at this:
