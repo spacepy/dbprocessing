1073449dce01357737e41ed16f7ac17dbee7b6be
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2017-06-27 10:07:13 -0400

    functional test script: get along happily w/o testDB

--- test_DB/scripts/functionalTest.sh
+++ test_DB/scripts/functionalTest.sh
@@ -1,4 +1,4 @@
-rm ../testDB.sqlite
+rm -f ../testDB.sqlite
 ~/dbUtils/CreateDB.py ../testDB.sqlite
 ~/dbUtils/addFromConfig.py -m ../testDB.sqlite ../config/testDB.conf
 ~/dbUtils/ProcessQueue.py -i -m ../testDB.sqlite
