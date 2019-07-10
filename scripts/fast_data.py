8171291715670a54d2b298bf6d900d0b1e45e365
Lorna Ellis <Lorna.Ellis@unh.edu>
2019-07-10 12:43:30 -0400

    Changes to get this working. Still includes debugging code.

--- scripts/fast_data.py
+++ scripts/fast_data.py
@@ -4,6 +4,7 @@ import datetime
 import itertools
 import os
 from optparse import OptionParser
+import pdb
 
 import networkx
 from dbprocessing import DButils, Version
@@ -51,29 +52,42 @@ def get_fastdata_participants(graph, cutoff):
     Returns:
         set(dict) -- Set of the file records
     """
-    l0 = [n for n, d in graph.in_degree() if d == 0]
-    fast0 = set([x for x in l0 if graph.nodes[x]['utc_file_date'] > cutoff])
+    #l0 = [n for n, d in graph.in_degree() if d == 0]
+    #fast0 = set([x for x in l0 if graph.nodes[x]['utc_file_date'] > cutoff])
+    l0 = [ii for ii in graph.in_degree() if graph.in_degree(ii) == 0]
+    fast0 = [ii[0] for ii in graph.nodes(True) if ii[0] in l0 and \
+             ii[1]['utc_file_date'] < cutoff]
     fast0_children_lists = [networkx.descendants(graph, x) for x in fast0]
     fast0_children = set(itertools.chain.from_iterable(fast0_children_lists))
 
-    return fast0_children | fast0
+    return fast0_children | set(fast0)
 
 
 def reap_files(graph, participants):
-    nodes = [graph.nodes[x] for x in participants]
+
+    nodes = [ii for ii in graph.nodes(True) if ii[0] in participants]
     nodes.sort(key=lambda file: (
-        file['product_id'], file['utc_file_date'], file['version']))
+        file[1]['product_id'], file[1]['utc_file_date'], file[1]['version']))
 
     last_node = nodes[0]
     for node in reversed(nodes):
-        if (node['product_id'] == last_node['product_id'] and
-                node['utc_file_date'] == last_node['utc_file_date'] and
-                node['version'] < last_node['version']):
-            os.remove(dbu.getFileFullPath(node['file_id']))
+        print("{} {}".format(node[0], dbu.getFileFullPath(node[1]['file_id'])))
+        print("{} {}".format(node[1]['product_id'], last_node[1]['product_id']))
+        print("{} {}".format(node[1]['utc_file_date'], last_node[1]['utc_file_date']))
+        print("{} {}".format(node[1]['version'], last_node[1]['version']))
+        print(" ")
+        if (node[1]['product_id'] == last_node[1]['product_id'] and
+                node[1]['utc_file_date'] == last_node[1]['utc_file_date'] and
+                node[1]['version'] < last_node[1]['version']):
+            pdb.set_trace()
+            os.remove(dbu.getFileFullPath(node[1]['file_id']))
             # This is slow, and we(I.E. not me) can fix it later if its too slow. - Myles 6/5/2018
-            dbu.getEntry('File', node['file_id']).exists_on_disk = False
+            dbu.getEntry('File', node[1]['file_id']).exists_on_disk = False
 
         last_node = node
+    print("finished")
+    pdb.set_trace()
+    print("here")
 
 
 def reap_records(graph, participants):
