32eb911c18335e0998f90eab683f47e13dee0f14
Jonathan Niehof <Jonathan.Niehof@unh.edu>
2019-08-20 14:36:52 -0400

    changeProductDir: change the directory of a product and move all files

--- setup.py
+++ setup.py
@@ -27,7 +27,8 @@ scripts = ('scripts/addFromConfig.py', 'scripts/missingFilesByProduct.py',
            'scripts/magephem_def_dataToIncoming.py', 'scripts/writeProcessConf.py',
            'scripts/magephem-pre-CoverageHTML.py', 'scripts/writeProductsConf.py',
            'scripts/makeLatestSymlinks.py', 'scripts/testInspector.py',
-           'scripts/replaceArgsWithRootdir.py', 'scripts/printRequired.py')
+           'scripts/replaceArgsWithRootdir.py', 'scripts/printRequired.py',
+           'scripts/changeProductDir.py')
 
 setup(name='dbprocessing',
       version='0.0',
