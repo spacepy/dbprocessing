55729371bf80bc037bfed70415f49e43b41ccf5f
Jon Niehof <jtniehof@gmail.com>
2021-11-09 14:08:18 -0500

    sphinx config: remove redundant TODO definitions

--- docs/conf.py
+++ docs/conf.py
@@ -59,9 +59,6 @@ extensions = [
 autosummary_generate = True
 numpydoc_show_class_members = False
 
-# # make it so TODOs will work
-# todo_include_todos = True
-
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
