30c9323c51838d818de0cfa7cd9c0afbe958beca
Jon Niehof <jtniehof@gmail.com>
2022-02-04 14:30:58 -0500

    docs: change some title configuration to read smoother

--- docs/conf.py
+++ docs/conf.py
@@ -167,7 +167,7 @@ html_theme_options = { 'sidebarwidth': '350' }
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-#html_title = None
+html_title = "%s v%s manual" % (project, version)
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
